23097da65d9b1ade25741dc7a208df4711e1af4f
GSWNJay <88433600+GSWNJay@users.noreply.github.com>
2024-08-21 16:30:04 +0200

    Module 2 Continued
    The rest of the labs and coding practice pages from module 2.

---
+++ Mod2_P2.py
@@ -0,0 +1,25 @@
+S = {'A','B','C'}
+U = {'A','B','Z'}
+
+U.union(S)
+print(U)
+
+S={'A','B','C'}
+U={'A','Z','C'}
+
+I = U&S
+print(I)
+
+#Quiz
+#1
+A = ('a','b','c')
+print(A[0])
+#2
+L=['c']
+L.append(['a','b'])
+
+print(len(L))
+#4
+Dict={"A":1,"B":"2","C":[3,3,3],"D":(4,4,4),'E':5,'F':6}
+
+print(Dict["D"])
